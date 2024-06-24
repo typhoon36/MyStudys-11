a138fc0476ce74b93fd49e744169ff3e3f7058fd
YEONJUN LEE <154724697+typhoon36@users.noreply.github.com>
2024-06-24 20:02:13 +0900

    MyStudys
    SpaceShooter
    - 씬 분리 병합 로딩 구현
    - 절대강좌 2021 리소스 임포트
    - 쿼터뷰 시점의 문제점 해결(에셋 반투명화)_Explan

---
+++ 58/02.Scripts/LobbyMgr.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyMgr : MonoBehaviour
+{
+    public Button m_Start_Btn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f; //일시정지를 원래 속도로...
+
+        if (m_Start_Btn != null)
+            m_Start_Btn.onClick.AddListener(StartBtnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartBtnClick()
+    {
+        SceneManager.LoadScene("ScLevel_01");
+        SceneManager.LoadScene("scPlay", LoadSceneMode.Additive);
+    }
+}
